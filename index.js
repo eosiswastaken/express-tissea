a14bf34e99bd0126308370855f058587d601aca1
Antoine Cerdan <eosiswastaken@gmail.com>
2025-04-08 22:26:26 +0200

    add jwt middleware

--- index.js
+++ index.js
@@ -2,14 +2,44 @@ import express from 'express';
 import statsRoutes from './routes/stats.js';
 import categoriesRoutes from './routes/categories.js';
 import userRoutes from './routes/user.js';
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
+dotenv.config()
+console.log(process.env.TOKEN_SECRET)
 const app = express();
 app.use(express.json())
 const port = 3000;
 
-app.use('/api/stats',statsRoutes);
-app.use('/api/categories',categoriesRoutes);
-app.use('/api/users',userRoutes);
+function generateAccessToken(hash) {
+  return jwt.sign(hash, process.env.TOKEN_SECRET);
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
+
+app.get("/token",(req,res) => {
+  const token = generateAccessToken("username")
+  res.json({"token":token})
+})
+
+app.use('/api/stats',authenticateToken,statsRoutes);
+app.use('/api/categories',authenticateToken,categoriesRoutes);
+app.use('/api/users',authenticateToken,userRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
